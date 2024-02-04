2e0edd0c26c4937e7efdb760798af5ec00400ae0
OBones <obones@free.fr>
2024-02-04 16:23:55 +0100

    Feat: partial update also deals with the battery

--- src/partialUpdate.cpp
+++ src/partialUpdate.cpp
@@ -11,12 +11,15 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 #include <time.h>
+#include <esp_adc_cal.h>
+#include <HardwareSerial.h>
 #include "screenDimensions.h"
 #include "timeManagement.h"
 #include "drawPrimitives.h"
 
 #define AREA_WIDTH 50
 #define AREA_HEIGHT 50
+#define BATT_PIN   (14)
 
 Rect_t partialUpdateArea = 
     {
@@ -31,19 +34,49 @@ void edp_partial_update()
     epd_draw_grayscale_image(partialUpdateArea, FrameBuffer); // Update the screen
 }
 
-void DoPartialUpdate()
+void refreshBattery()
 {
-    epd_poweron();      // Switch on EPD display
-    epd_clear_area(partialUpdateArea);
+    esp_adc_cal_characteristics_t adc_chars;
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(
+        ADC_UNIT_2,
+        ADC_ATTEN_DB_11,
+        ADC_WIDTH_BIT_12,
+        1100,
+        &adc_chars
+    );
 
+    int vref = 1100;
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) 
+    {
+        Serial.printf("eFuse Vref: %umV\r\n", adc_chars.vref);
+        vref = adc_chars.vref;
+    }
+
+    uint16_t v = analogRead(BATT_PIN);
+    float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
+
+    drawString(partialUpdateArea.x, partialUpdateArea.y + partialUpdateArea.height / 2, String(battery_voltage), RIGHT);
+}
+
+void refreshTime()
+{
     struct tm timeInfo;
     timeInfo.tm_hour = CurrentHour;
     timeInfo.tm_min = CurrentMin;
     timeInfo.tm_sec = CurrentSec;
 
     char updateTime[30];
-    strftime(updateTime, sizeof(updateTime), "%H:%M:%S", &timeInfo);  // Creates: '@ 14:05:49'   and change from 30 to 8 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    drawString(partialUpdateArea.x, partialUpdateArea.y + partialUpdateArea.height / 2, updateTime, RIGHT);
+    strftime(updateTime, sizeof(updateTime), "%H:%M:%S", &timeInfo);
+    drawString(partialUpdateArea.x, partialUpdateArea.y, updateTime, RIGHT);
+}
+
+void DoPartialUpdate()
+{
+    epd_poweron();      // Switch on EPD display
+    epd_clear_area(partialUpdateArea);
+
+    refreshBattery();
+    refreshTime();
 
     edp_partial_update();       // Update the display to show the information
     epd_poweroff_all(); // Switch off all power to EPD
