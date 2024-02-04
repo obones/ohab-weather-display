91755614f924e6b83fe937249e7d0c6343a502a2
OBones <obones@free.fr>
2024-02-04 14:29:25 +0100

    fix: add license

--- include/partialUpdate.h
+++ include/partialUpdate.h
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2024 Olivier Sannier
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. 
+ * If a copy of the MPL was not distributed with this file, 
+ * you can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
 #ifndef PARTIAL_UPDATE_H
 #define PARTIAL_UPDATE_H
 
