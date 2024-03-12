341c883b518e606f9087f427d1c6d89ca872c996
OBones <obones@free.fr>
2024-03-12 10:20:44 +0100

    fix: to allow #including the constants.h file multiple times, the variables/consts must be defined only once in a CPP file

--- include/constants.h
+++ include/constants.h
@@ -10,35 +10,16 @@
  *
  * SPDX-License-Identifier: MPL-2.0
  */
+#pragma once
 
-// If you get an error on this line, please create the file according to the sample file
-#include "credentials.h"
+extern const char* ssid;
+extern const char* password;
 
-const char* ssid     = WIFI_SSID;
-const char* password = WIFI_PASSWORD;
+extern const char* OpenHABServerName;
+extern const int OpenHABServerPort;
+extern const char* OpenHABItemName;
 
-const char* OpenHABServerName = OPENHAB_SERVER_NAME;
-const int OpenHABServerPort = OPENHAB_SERVER_PORT;
-const char* OpenHABItemName = OPENHAB_ITEM_NAME;
-
-const char* Timezone = TIMEZONE;  // Choose your time zone from: https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv 
-                                                           // See below for examples
-const char* ntpServer   = NTP_SERVER;             // Or, choose a time server close to you, but in most cases it's best to use pool.ntp.org to find an NTP server
-                                                           // then the NTP system decides e.g. 0.pool.ntp.org, 1.pool.ntp.org as the NTP syem tries to find  the closest available servers
-                                                           // EU "0.europe.pool.ntp.org"
-                                                           // US "0.north-america.pool.ntp.org"
-                                                           // See: https://www.ntppool.org/en/                                                           
-int gmtOffset_sec     = GMT_OFFSET_SEC;    // UK normal time is GMT, so GMT Offset is 0, for US (-5Hrs) is typically -18000, AU is typically (+8hrs) 28800
-int daylightOffset_sec = DAYLIGHT_OFFSET_SEC; // In the UK DST is +1hr or 3600-secs, other countries may use 2hrs 7200 or 30-mins 1800 or 5.5hrs 19800 Ahead of GMT use + offset behind - offset
-
-// Example time zones
-//const char* Timezone = "MET-1METDST,M3.5.0/01,M10.5.0/02"; // Most of Europe
-//const char* Timezone = "CET-1CEST,M3.5.0,M10.5.0/3";       // Central Europe
-//const char* Timezone = "EST-2METDST,M3.5.0/01,M10.5.0/02"; // Most of Europe
-//const char* Timezone = "EST5EDT,M3.2.0,M11.1.0";           // EST USA  
-//const char* Timezone = "CST6CDT,M3.2.0,M11.1.0";           // CST USA
-//const char* Timezone = "MST7MDT,M4.1.0,M10.5.0";           // MST USA
-//const char* Timezone = "NZST-12NZDT,M9.5.0,M4.1.0/3";      // Auckland
-//const char* Timezone = "EET-2EEST,M3.5.5/0,M10.5.5/0";     // Asia
-//const char* Timezone = "ACST-9:30ACDT,M10.1.0,M4.1.0/3":   // Australia
-
+extern const char* Timezone;
+extern const char* ntpServer;
+extern int gmtOffset_sec;
+extern int daylightOffset_sec;
---
+++ src/constants.cpp
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2024 Olivier Sannier
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. 
+ * If a copy of the MPL was not distributed with this file, 
+ * you can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+#include "constants.h"
+
+// If you get an error on this line, please create the file according to the sample file
+#include "credentials.h"
+
+const char* ssid     = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
+
+const char* OpenHABServerName = OPENHAB_SERVER_NAME;
+const int OpenHABServerPort = OPENHAB_SERVER_PORT;
+const char* OpenHABItemName = OPENHAB_ITEM_NAME;
+
+const char* Timezone = TIMEZONE;  // Choose your time zone from: https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv 
+                                                           // See below for examples
+const char* ntpServer   = NTP_SERVER;             // Or, choose a time server close to you, but in most cases it's best to use pool.ntp.org to find an NTP server
+                                                           // then the NTP system decides e.g. 0.pool.ntp.org, 1.pool.ntp.org as the NTP syem tries to find  the closest available servers
+                                                           // EU "0.europe.pool.ntp.org"
+                                                           // US "0.north-america.pool.ntp.org"
+                                                           // See: https://www.ntppool.org/en/                                                           
+int gmtOffset_sec     = GMT_OFFSET_SEC;    // UK normal time is GMT, so GMT Offset is 0, for US (-5Hrs) is typically -18000, AU is typically (+8hrs) 28800
+int daylightOffset_sec = DAYLIGHT_OFFSET_SEC; // In the UK DST is +1hr or 3600-secs, other countries may use 2hrs 7200 or 30-mins 1800 or 5.5hrs 19800 Ahead of GMT use + offset behind - offset
+
+// Example time zones
+//const char* Timezone = "MET-1METDST,M3.5.0/01,M10.5.0/02"; // Most of Europe
+//const char* Timezone = "CET-1CEST,M3.5.0,M10.5.0/3";       // Central Europe
+//const char* Timezone = "EST-2METDST,M3.5.0/01,M10.5.0/02"; // Most of Europe
+//const char* Timezone = "EST5EDT,M3.2.0,M11.1.0";           // EST USA  
+//const char* Timezone = "CST6CDT,M3.2.0,M11.1.0";           // CST USA
+//const char* Timezone = "MST7MDT,M4.1.0,M10.5.0";           // MST USA
+//const char* Timezone = "NZST-12NZDT,M9.5.0,M4.1.0/3";      // Auckland
+//const char* Timezone = "EET-2EEST,M3.5.5/0,M10.5.5/0";     // Asia
+//const char* Timezone = "ACST-9:30ACDT,M10.1.0,M4.1.0/3":   // Australia
