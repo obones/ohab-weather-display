ea4b33721f5372269c179b346c710ee78353d1bf
OBones <obones@free.fr>
2024-03-12 16:33:46 +0100

    feat: warn if the battery is too low and never wakeup under these conditions

--- src/main.cpp
+++ src/main.cpp
@@ -23,21 +23,27 @@
 #include "screenDimensions.h"
 #include "drawPrimitives.h"
 #include "timeManagement.h"
+#include "batteryManagement.h"
+#include "lang.h"
 #include "fontManagement.h"
 #include "fonts/opensans26b.h"
 
 long StartTime       = 0;
 
-void BeginSleep() 
+void BeginSleep(bool allowWakeUp) 
 {
     Serial.println("Awake for : " + String((millis() - StartTime) / 1000.0, 3) + "-secs");
     Serial.println("Starting deep-sleep period...");
-    esp_err_t resultCode = esp_sleep_enable_ext0_wakeup(GPIO_NUM_9, 0);
-    if (resultCode != ESP_OK)
+    if (allowWakeUp)
     {
-        setFont(OpenSans26B);
-        drawString(10, SCREEN_HEIGHT / 2, String("EXT0 issue: ") + String(resultCode), LEFT);
+        esp_err_t resultCode = esp_sleep_enable_ext0_wakeup(GPIO_NUM_9, 0);
+        if (resultCode != ESP_OK)
+        {
+            setFont(OpenSans26B);
+            drawString(10, SCREEN_HEIGHT / 2, String("EXT0 issue: ") + String(resultCode), LEFT);
+        }
     }
+
     epd_poweroff_all();
 
     esp_deep_sleep_start();  // Sleep until we have received a GPIO interrupt
@@ -75,32 +81,67 @@ void setup()
 {
     InitializeSystem();
 
-    esp_reset_reason_t resetReason = esp_reset_reason();
-    if (resetReason == ESP_RST_DEEPSLEEP)
+    bool lowBattery = BatteryManagement::GetBatteryVoltage() < 2.99;
+    if (lowBattery)
     {
-        TimeManagement::RetrieveTime();
+        epd_clear();
 
-        time_t now;
-        struct tm  info;
-        time(&now);
-        localtime_r(&now, &info);
+        const int bodyWidth = 200;
+        const int rectLeft = (SCREEN_WIDTH - bodyWidth) / 2;
+        const int rectHeight = 50;
+        const int bodyTop = (SCREEN_HEIGHT - rectHeight) / 2;
+        const int lineMargin = 20;
+        const int tipWidth = 10;
+        const int tipHeight = 20;
+        const int lineStartLeft = (SCREEN_WIDTH - rectHeight) / 2 - lineMargin;
+        const int lineStartTop = (SCREEN_HEIGHT + rectHeight) / 2 + lineMargin;
+        const int lineEndLeft = lineStartLeft + rectHeight + 2 * lineMargin;
+        const int lineEndTop = lineStartTop - (rectHeight + 2 * lineMargin);
 
-        if (info.tm_min % 5 == 0)
-        {
-            DoFullUpdate((info.tm_hour == 4) && (info.tm_min < 2));
-        }
-        else
-        {
-            DoPartialUpdate();
-        }
+        drawRect(rectLeft, bodyTop, bodyWidth, rectHeight, Black);
+        drawRect(rectLeft + 1, bodyTop + 1, bodyWidth - 2, rectHeight - 2, Black);
+
+        drawRect(rectLeft + bodyWidth - 1, bodyTop + (rectHeight - tipHeight) / 2, tipWidth, tipHeight, Black);
+        drawRect(rectLeft + bodyWidth - 1, bodyTop + (rectHeight - tipHeight) / 2 + 1, tipWidth - 2, tipHeight - 2, Black);
+
+        drawLine(lineStartLeft, lineStartTop, lineEndLeft, lineEndTop, Black);
+        drawLine(lineStartLeft - 1, lineStartTop, lineEndLeft - 1, lineEndTop, Black);
+        drawLine(lineStartLeft + 1, lineStartTop, lineEndLeft + 1, lineEndTop, Black);
+
+        setFont(OpenSans26B);
+        drawString(SCREEN_WIDTH / 2, 50, Lang::Status::LowBattery, CENTER);
+
+        epd_draw_grayscale_image(epd_full_screen(), FrameBuffer); // Update the screen
     }
     else
     {
-        TimeManagement::StartTimer();
-        DoFullUpdate(true);
+        esp_reset_reason_t resetReason = esp_reset_reason();
+        if (resetReason == ESP_RST_DEEPSLEEP)
+        {
+            TimeManagement::RetrieveTime();
+
+            time_t now;
+            struct tm  info;
+            time(&now);
+            localtime_r(&now, &info);
+
+            if (info.tm_min % 5 == 0)
+            {
+                DoFullUpdate(info.tm_min % 15 == 0); //(info.tm_hour == 4) && (info.tm_min < 2));
+            }
+            else
+            {
+                DoPartialUpdate();
+            }
+        }
+        else
+        {
+            TimeManagement::StartTimer();
+            DoFullUpdate(true);
+        }
     }
-    
-    BeginSleep();
+
+    BeginSleep(!lowBattery);
 }
 
 
